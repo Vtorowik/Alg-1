87b504206622c5f36b3b9cf95af3b5ef548ff901
Vtorow <mosmirea@mail.ru>
2023-04-17 15:18:19 +0300

    Update

--- Alg-1/main.cpp
+++ Alg-1/main.cpp
@@ -69,12 +69,59 @@ Node* Find_ch1(Node* TRE,int ch1){
     }
 }
 
+void DeleteNode(Node* TRE, int ch1) {
+    Node *curr = TRE;
+    Node *parent = NULL;
+    while (curr != NULL && curr->UZEL != ch1) {
+        parent = curr;
+        if (curr->UZEL > ch1) {
+            curr = curr->left;
+        } else {
+            curr = curr->right;
+        }
+    }
+    if (curr = NULL) {
+        return;
+    }
+    bool IsLeftChild = (parent != NULL && curr == parent->left);
+    if (curr->left == nullptr && curr->right == nullptr) {
+        if (parent == NULL) {
+            TRE = NULL;
+        } else if (IsLeftChild) {
+            parent->left = NULL;
+        } else {
+            parent->right = NULL;
+        }
+        delete curr;
+    } else if (curr->left == nullptr || curr->right == nullptr) {
+        Node *child = (curr->left != NULL) ? curr->left : curr->right;
+        if (parent == NULL) {
+            TRE = child;
+        } else if (IsLeftChild) {
+            parent->left = child;
+        } else {
+            parent->right = child;
+        }
+        delete curr;
+    } else {
+        Node *successor = curr->right;
+        while (successor->left != NULL) {
+            successor = successor->left;
+        }
+        int successorCh1 = successor->UZEL;
+        DeleteNode(TRE, successorCh1);
+        curr->UZEL = successorCh1;
+    }
+}
+
 int main(){
     Node* Treant = createNode(2);
     int arr[7] = {1,4,6,7,77,8,99};
     for (int i=0; i < 7; i++){
         Set_Node(Treant, arr[i]);
     }
+    cout << "Tree: ";
+
     Get_Tree(Treant);
     cout << endl;
     cout << Depth_Tree(Treant) << endl;
